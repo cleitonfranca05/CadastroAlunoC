0138f8cc132cbb72ad809255155365ad747dd592
Cleiton Faria França <cleitonfranca05@gmail.com>
2025-11-22 11:30:29 -0300

    Criando o Main . c
    Criando o Main com o Menu

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "alunos.h"
+
+int main() {
+    Aluno turma[MAX_ALUNOS];
+    int total = 0;
+    int opcao;
+
+    //Cleiton
+    carregar_turma(turma, &total);
+
+    printf("\n---  #####  -----   Sistema de Alunos  -----  #####   ---\n");
+    printf("1 - Cadastrar novo aluno \n");
+    printf("2 - Listar todos alunos \n");
+    printf("3 - Salvar turma \n");
+    printf("0 - Sair \n");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
+
+
+
+    return 0;
+}
