42427bf3dabad38848939827288952001cae3d3d
LavigniaSilva <lavignia.silva16@gmail.com>
2025-11-24 21:43:19 -0300

    Menu atualizado

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main() {
     carregar_turma(turma, &total);
 
     // Lavignia
+    do{
     printf("\n---  #####  -----   Sistema de Alunos  -----  #####   ---\n");
     printf("1 - Cadastrar novo aluno \n");
     printf("2 - Listar todos alunos \n");
@@ -18,7 +19,29 @@ int main() {
     printf("Opcao: ");
     scanf("%d", &opcao);
 
+    switch(opcao){
 
+    case 1:
+         cadastrar_aluno(turma, &total);
+         break;
+
+    case 2:
+         listar_alunos(turma, &total);
+         break;
+
+    case 3:
+        salvar_turma(turma, total);
+        break;
+
+    case 0:
+       printf("Saindo do sistema...\n");
+        break;
+
+    default:
+        printf("Opção inválida! Tente novamente.\n");
+        }
+
+    } while (opcao != 0);
 
     return 0;
 }
